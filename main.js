6f564723c8a3c3ea21102d712960346d9462ecf8
yuukiflow <yuukiflow@gmail.com>
2024-04-08 23:44:19 +0200

    added error handling

--- main.js
+++ main.js
@@ -12,31 +12,41 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-    console.log("Message detected : " + message.channelId + "Compared to " + process.env.SOURCE_CHANNEL_ID);
-    if (message.attachments.size === 0) return;
-    if (message.channelId === process.env.SOURCE_CHANNEL_ID.trim()) {
-        console.log("Channel Found");
-        let url = message.attachments.first().url;
-        let collector = message.createReactionCollector();
-        collector.on("collect", Reaction => DetectReaction(Reaction, url));
+    try {
+        console.log("Message detected : " + message.channelId + "Compared to " + process.env.SOURCE_CHANNEL_ID);
+        if (message.attachments.size === 0) return;
+        if (message.channelId === process.env.SOURCE_CHANNEL_ID.trim()) {
+            console.log("Channel Found");
+            let url = message.attachments.first().url;
+            let collector = message.createReactionCollector();
+            collector.on("collect", Reaction => DetectReaction(Reaction, url));
+        }
+    } catch (error) {
+        console.error("An error occurred while processing message:", error);
     }
 });
 
 async function DetectReaction(Reaction, url) {
-    console.log("Reaction detected!");
-    if (process.env.EMOJI != "") {
-        if (Reaction.emoji.name === process.env.EMOJI) {
+    try {
+        console.log(Reaction.emoji.toString());
+        if (process.env.EMOJI != "") {
 
+            if (Reaction.emoji.toString() === process.env.EMOJI.trim()) {
+                Reaction.message.delete();
+                let channel = await client.channels.fetch(process.env.DESTINATION_CHANNEL_ID);
+                channel.send(url);
+            }
+        } else {
             Reaction.message.delete();
             let channel = await client.channels.fetch(process.env.DESTINATION_CHANNEL_ID);
             channel.send(url);
-
         }
-    } else {
-        Reaction.message.delete();
-        let channel = await client.channels.fetch(process.env.DESTINATION_CHANNEL_ID);
-        channel.send(url);
+    } catch (error) {
+        console.error("An error occurred while detecting reaction:", error);
     }
 }
 
-client.login(process.env.CLIENT_TOKEN);
+client.login(process.env.CLIENT_TOKEN).catch(error => {
+    console.error("An error occurred while logging in:", error);
+});
+
